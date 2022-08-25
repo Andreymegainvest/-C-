75ed203560bbd890b36851dd6d4866d4842ac65b
Andrey <megainvest@yandex.ru>
2022-08-25 18:27:47 +0300

    Сделал последнюю задачу

--- Program.cs
+++ Program.cs
@@ -83,14 +83,62 @@
 //     i++;
 // }
 
-Console.Clear();
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
 
-Console.WriteLine("Задача 10 ");
 
-Console.WriteLine("Введите трёхзначное число: ");
+// Console.WriteLine("Задача 10 ");
 
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите трёхзначное число: ");
 
-string stringNumber = Convert.ToString(threeDigitNumber);
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("вторая цифра этого числа = " + stringNumber[1]);
+// string stringNumber = Convert.ToString(threeDigitNumber);
+
+// Console.WriteLine("вторая цифра этого числа = " + stringNumber[1]);
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.WriteLine("Введите число: ");
+
+// int anyNumber = Convert.ToInt32(Console.ReadLine());
+
+// string anyNumberText = Convert.ToString(anyNumber);
+
+// if (anyNumberText.Length > 2)
+// {
+//     Console.WriteLine("третья цифра " + anyNumberText[2]);
+// }
+// else
+// {
+//     Console.WriteLine(" третьей цифры нет");
+// }
+
+// 
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.Write("Введите цифру, обозначающую день недели: ");
+
+// int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+// void CheckingTheDayofTheWeek (int dayNumber)
+//  {
+//     if (dayNumber == 6 || dayNumber ==7)
+//     {
+//        Console.WriteLine("(этот день выходной)  да"); 
+//     } 
+// else if (dayNumber < 1 || dayNumber > 7)
+// {
+//     Console.WriteLine("это вообще не день недели");
+// }
+// else Console.WriteLine("(этот день не выходной) нет");
+// }
+// CheckingTheDayofTheWeek(dayNumber);
